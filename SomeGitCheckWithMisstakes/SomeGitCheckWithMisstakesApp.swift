af91720e501d4aed4184d3f0301262dee53849ee
Volodimer1987 <kynicin25121987@gmail.com>
2024-05-21 10:22:06 +0300

    Initial Commit

---
+++ SomeGitCheckWithMisstakes/SomeGitCheckWithMisstakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomeGitCheckWithMisstakesApp.swift
+//  SomeGitCheckWithMisstakes
+//
+//  Created by vladimir gennadievich on 21.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SomeGitCheckWithMisstakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
