c2035322aef04576b6400d85454e454b07d72bc6
Volodimer1987 <kynicin25121987@gmail.com>
2024-05-24 22:10:39 +0300

    commit is ready now no conflict

--- SomeGitCheckWithMisstakes/SomeFolderCheckBranch.swift
+++ SomeGitCheckWithMisstakes/SomeFolderCheckBranch.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct SomeFolderCheckBranch: View {
+    @State private var someCheckChange = ""
+    
     var body: some View {
         ZStack {
             Image("god")
@@ -15,10 +17,25 @@ struct SomeFolderCheckBranch: View {
                 .aspectRatio(contentMode: .fill)
                 .ignoresSafeArea()
             
-            Text("This is from folder branch")
-                .foregroundStyle(Color.green)
-                .font(.largeTitle)
+            VStack(spacing: 20 , content: {
+                Text("This is from folder branch")
+                    .foregroundStyle(Color.green)
+                    .font(.largeTitle)
+                
+                Text(someCheckChange)
+                    .font(.title)
+                    .foregroundStyle(.yellow)
+            })
         }
+        .task {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+                someCheckChange = "This is new lesson from nick"
+            })
+            DispatchQueue.global().asyncAfter(deadline: .now() + 5) {
+                someCheckChange = ""
+            }
+        }
+        
     }
 }
 
